68de3beba68056695dd917c9b2cb2c6bae91bb88
chob1976 <104876234+chob1976@users.noreply.github.com>
2023-07-05 17:13:57 -0600

    GUlp file created

--- cardGame/gulpfile.js
+++ cardGame/gulpfile.js
@@ -1,13 +1,20 @@
 const rollup = require('rollup');
 const gulp = require('gulp');
 const clean = require('gulp-clean');
+const browsersynch = require('browser-sync');
+const server = browsersynch.create();
 
 exports.bundle = () => {
     console.log('GULP - Running bundle function');
 }
 
 exports.play = () => {
-    console.log('GULP - Running play function')
+    console.log('GULP - Running game....');
+    server.init({
+        server: {
+            baseDir: "."
+        }
+    })
 }
 
 gulp.task('clean', () => {
