a2396b320f5cd5f8828e4d83ab36cb296d35bc72
chob1976 <104876234+chob1976@users.noreply.github.com>
2023-07-26 19:32:58 -0600

    take off the hack on dificulty

--- cardGame/js/controllers/play/playservice.js
+++ cardGame/js/controllers/play/playservice.js
@@ -8,8 +8,8 @@ export class PlayService {
         var cards = [];
         //var url = '../../../data/cards.json';
         //var url = 'https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/8/type/flags'
-        var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/${2}/type/${theme}`;
-        //var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/${difficulty}/type/${theme}`;
+        //var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/${2}/type/${theme}`;
+        var url = `https://us-central1-cenfoprojectsbackend.cloudfunctions.net/app/cards/${difficulty}/type/${theme}`;
         var request = new XMLHttpRequest();
         request.open('get', url);
         request.onload = () => {
